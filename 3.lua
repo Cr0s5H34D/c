6e4bb9c0ea3774fad2c81522840ca55da880bd8b
Cr0s5H34D <70684668+Cr0s5H34D@users.noreply.github.com>
2023-01-09 19:46:47 -0600

    3.lua

---
+++ 3.lua
@@ -0,0 +1,16 @@
+while true do
+    wait(0.5)
+        game.StarterGui:SetCore("SendNotification", {
+        Title = "YOU ARE";
+        Text = "AN IDIOT";
+        Icon = "rbxassetid://12109250396";
+        Duration = 2;
+        })
+        
+        game.StarterGui:SetCore("SendNotification", {
+        Title = "YOU ARE";
+        Text = "AN IDIOT";
+        Icon = "rbxassetid://12109251356";
+        Duration = 2;
+        })        
+    end
